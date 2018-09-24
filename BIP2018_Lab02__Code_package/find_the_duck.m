ea95b4d4bbb652cebce05504577f3b210e4c3773
kiskompi <kiskompi@gmail.com>
2018-09-24 13:48:53 +0200

    final

--- BIP2018_Lab02__Code_package/find_the_duck.m
+++ BIP2018_Lab02__Code_package/find_the_duck.m
@@ -1,5 +1,6 @@
 function duck = find_the_duck(I)
 	red  = I(:,:,1);
     blue = I(:,:,2);
-    duck = logical (red > 50);
+    green= I(:,:,3);
+    duck = blue - red < 33 | (red < 45 & blue < 45 & green < 45) | (red > 50 & red > blue) ;
 end
--- BIP2018_Lab02__Code_package/find_the_pine.m
+++ BIP2018_Lab02__Code_package/find_the_pine.m
@@ -4,7 +4,11 @@ function pine = find_the_pine(I)
    sat = hsv(:,:,2);
    val = hsv(:,:,3);
    
-   pineh =  logical (hue < 0.15);
-   pines = logical (sat > 0.1);
-   pine = pineh & pines;
+   pineh = logical (hue < 0.15);
+   pines = logical (sat < 0.2);
+   pinev = val > 0.025 & val < 0.95;
+   pine  = imfill((pineh | pines) & pinev , 'holes');
+   se = [0 1 0; 1 1 1; 0 1 0];
+   
+   pine = imerode(pine, se);
 end
